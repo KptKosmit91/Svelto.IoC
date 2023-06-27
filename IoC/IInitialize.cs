f71fc3329aaac93df9718204ec58d1ca6eb486ff
KptKosmit91 <kptkosmit91@o2.pl>
2023-06-27 19:18:26 +0200

    Update IInitialize.cs

--- IoC/IInitialize.cs
+++ IoC/IInitialize.cs
@@ -1,9 +1,13 @@
 using System;
 
+#if !SIMPLIFY_SVELTO_NAMESPACES
 namespace Svelto.IoC
 {
-    interface IInitialize
+#endif
+	interface IInitialize
 	{
 		void OnDependenciesInjected();
 	}
+#if !SIMPLIFY_SVELTO_NAMESPACES
 }
+#endif
