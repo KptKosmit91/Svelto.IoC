183c3f3d80202f00a20090002e2afb48d841b204
KptKosmit91 <kptkosmit91@o2.pl>
2023-06-27 20:15:36 +0200

    Update ContextContainer.cs

--- IoC/Extensions/Context/ContextContainer.cs
+++ IoC/Extensions/Context/ContextContainer.cs
@@ -7,11 +7,68 @@ using Svelto.Context;
 
 namespace Svelto.IoC.Extensions.Context
 {
-    public class ContextContainer: Container
+    public class ContextContainer : Container
     {
+        private class ContextBinder<Contractor> : IBinder<Contractor> where Contractor : class
+        {
+            private Action<Type> _onRegister;
+
+            private IInternalContainer _container;
+
+            private Type _interfaceType;
+
+            public ContextBinder(Action<Type> onRegister)
+            {
+                _onRegister = onRegister;
+            }
+
+            public void Bind<ToBind>(IInternalContainer container) where ToBind : class
+            {
+                _container = container;
+                _interfaceType = typeof(ToBind);
+            }
+
+            public void AsSingle<T>() where T : Contractor, new()
+            {
+                _container.Register<T, StandardProvider<T>>(_interfaceType, new StandardProvider<T>());
+                OnRegister(typeof(T));
+            }
+
+            public void AsInstance<T>(T instance) where T : class, Contractor
+            {
+                _container.Register<T, SelfProvider<T>>(_interfaceType, new SelfProvider<T>(instance));
+                OnRegister(typeof(T));
+            }
+
+            public void ToProvider<T>(IProvider<T> provider) where T : class, Contractor
+            {
+                _container.Register<T, IProvider<T>>(_interfaceType, provider);
+                OnRegister(typeof(T));
+            }
+
+            private void OnRegister(Type implementationType)
+            {
+                if (typeof(IOnFrameworkInitialized).IsAssignableFrom(implementationType) || typeof(IOnFrameworkDestroyed).IsAssignableFrom(implementationType))
+                {
+                    _onRegister(_interfaceType);
+                }
+            }
+        }
+
         public ContextContainer(IContextNotifer contextNotifier)
         {
             _contextNotifier = contextNotifier;
+        }
+
+        protected override IBinder<TContractor> BinderProvider<TContractor>()
+        {
+            return new ContextBinder<TContractor>(AddType);
+        }
+
+        private void AddType(Type type)
+        {
+            var instance = Get(type);
+            OnInstanceGenerated(instance);
         }
 
         override protected void OnInstanceGenerated<TContractor>(TContractor instance)
@@ -26,4 +83,3 @@ namespace Svelto.IoC.Extensions.Context
         IContextNotifer _contextNotifier;
     }
 }
-
