b21ecaf5a9b9264e5781ec5fd229524db1798148
KptKosmit91 <kptkosmit91@o2.pl>
2023-06-28 15:08:57 +0200

    Update InjectAttribute.cs

--- IoC/InjectAttribute.cs
+++ IoC/InjectAttribute.cs
@@ -1,10 +1,14 @@
 using System;
 
+#if !SIMPLIFY_SVELTO_NAMESPACES
 namespace Svelto.IoC
 {
+#endif
 	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 	public class InjectAttribute: Attribute
 	{
         public string name { get; set; }
 	}
+#if !SIMPLIFY_SVELTO_NAMESPACES
 }
+#endif
