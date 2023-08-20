4c09f5926714ea7abaf70281effcb9d8908c7a4e
KptKosmit91 <kptkosmit91@o2.pl>
2023-08-20 12:22:12 +0200

    Create TypeUtility.cs

---
+++ Utilities/TypeUtility.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Svelto.IoC
+{
+    public static class TypeUtility
+    {
+        public static bool InheritsType(this Type baseType, Type implementationType)
+        {
+            return implementationType.IsAssignableFrom(baseType);
+        }
+    }
+}
